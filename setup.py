9d1ae9354b5000706520ef8254253c9e8876061d
khaingsuthway <khaingsuthway@gmail.com>
2024-11-30 21:54:36 +0700

    Add setup file

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+# Read README.md for long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ecg-analysis-tool",
+    version="0.1.0",
+    author="Khaing",
+    author_email="khaingsuthway@gmail.com",
+    description="A web application for ECG signal analysis and rhythm segmentation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KhaingSuThway/Descriptive_Analysis_of_ECG_Recordl",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+    ],
+    python_requires=">=3.11",
+    install_requires=[
+        "streamlit>=1.40.2",
+        "wfdb>=4.1.2",
+        "numpy>=1.24.0",
+        "polars>=0.20.0",
+        "matplotlib>=3.7.0",
+        "neurokit2>=0.2.0"
+    ],
+    entry_points={
+        "console_scripts": [
+            "ecg-analysis=streamlitApp:main",
+        ],
+    },
+    project_urls={
+        "Bug Reports": "https://github.com/KhaingSuThway/Descriptive_Analysis_of_ECG_Record",
+        "Source": "https://github.com/KhaingSuThway/Descriptive_Analysis_of_ECG_Record",
+        "Documentation": "https://github.com/KhaingSuThway/Descriptive_Analysis_of_ECG_Record#readme",
+    },
+)
